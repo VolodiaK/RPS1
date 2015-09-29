46026f42f525338b7a12cf62f382d511651aec0a
VolodiaK <klimenkovolodia.o@gmail.com>
2015-09-29 14:51:20 +0300

    Release

--- main.cpp
+++ main.cpp
@@ -1,13 +1,86 @@
 #include <stdlib.h>
-//#include <time.h>
 #include <iostream>
+#include <conio.h>
 
 using namespace std;
 
 int main()
 {
-    cout<<rand()%3<<endl;
-    system("pause");
+    int a=0;
+    int player, bot;
+    while(a!=2)
+    {
+        system("cls");
+        cout << "Rock -- 1" << endl;
+        cout << "Paper - 2" << endl;
+        cout << "Scissors - 3" << endl;
+        cout << "Your choice: ";
+        cin >> player;
+        bot = rand() % 3+1;
+        cout << "Bot -        " << bot << endl;
+        if(player == 1)
+        {
+            if(bot == 2)
+            {
+                cout << endl << "LOSER!!!!";
+            }
+            else
+            {
+                if(bot == 3)
+                {
+                    cout << endl << "WIN !!!";
+                }
+                else
+                {
+                    cout << "Try again";
+                }
+            }
+        }
+        else
+        {
+            if(player == 2)
+            {
+                if(bot == 3)
+                {
+                    cout << endl << "LOSER!!!!";
+                }
+                else
+                {
+                    if(bot == 1)
+                    {
+                        cout << endl << "WIN !!!";
+                    }
+                    else
+                    {
+                        cout << "Try again";
+                    }
+                }
+            }
+            else
+            {
+                if(bot == 1)
+                {
+                    cout << endl << "LOSER!!!!";
+                }
+                else
+                {
+                    if(bot == 2)
+                    {
+                        cout << endl << "WIN !!!";
+                    }
+                    else
+                    {
+                        cout << "Try again";
+                    }
+                }
+            }
+        }
+        getch();
+        system("cls");
+        cout << "New game -- 1" << endl;
+        cout << "Exit ------ 2" << endl;
+        cin >> a;
+    }
     return 0;
 }
 
