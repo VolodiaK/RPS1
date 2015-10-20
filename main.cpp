f4bacc52eb00f11fcb63d4a4ffc7438abaa01c29
VolodiaK <klimenkovolodia.o@gmail.com>
2015-10-20 13:40:10 +0300

    add enum

--- main.cpp
+++ main.cpp
@@ -5,40 +5,40 @@
 
 using namespace std;
 
-void RSP(string choise)
+void RSP(int choice)
 {
     srand(NULL);
     srand(time(0));
-    int bot = rand()%3;
-    if (choise == "Rock")
+    int bot{rand()%3};
+    if (choice == 1)
     {
         switch (bot)
         {
         case (0):
-            cout << endl << "Rock: " << "Draw!" << endl;
+            cout << endl << "Bot: Rock: " << "Draw!" << endl;
             break;
         case (1):
-            cout << endl << "Scissors: " << "Win!" << endl;
+            cout << endl << "Bot: Scissors: " << "Win!" << endl;
             break;
         case (2):
-            cout << endl << "Paper: " << "Loss!" << endl ;
+            cout << endl << "Bot: Paper: " << "Loss!" << endl ;
             break;
         }
     }
     else
     {
-        if (choise == "Scissors")
+        if (choice == 2)
         {
             switch (bot)
             {
             case (1):
-                cout << endl << "Scissors: " << "Draw!" << endl;
+                cout << endl << "Bot: Scissors: " << "Draw!" << endl;
                 break;
             case (2):
-                cout << endl << "Paper: " << "Win!" << endl;
+                cout << endl << "Bot: Paper: " << "Win!" << endl;
                 break;
             case (0):
-                cout << endl << "Rock: " << "Loss!" << endl;
+                cout << endl << "Bot: Rock: " << "Loss!" << endl;
                 break;
             }
         }
@@ -47,41 +47,53 @@ void RSP(string choise)
             switch (bot)
             {
             case (2):
-                cout << endl << "Paper: " << "Draw!" << endl;
+                cout << endl << "Bot: Paper: " << "Draw!" << endl;
                 break;
             case (0):
-                cout << endl << "Rock: " << "Win!" << endl;
+                cout << endl << "Bot: Rock: " << "Win!" << endl;
                 break;
             case (1):
-                cout << endl << "Scissors: " << "Loss!" << endl;
+                cout << endl << "Bot: Scissors: " << "Loss!" << endl;
                 break;
             }
         }
     }
 }
 
-int number(int a, char *b[])
+int number(int a,char *b[])
 {
-    for(int i=0; i<a; i++)
+    for (int i = 0 ;i<a; i++)
     {
-        if(b[i][0] == '-' && b[i][1] == 't')
+        char *temp;
+        temp = b[i];
+        if((temp[0] == '-')&&(temp[1] == 't'))
         {
-            int t = atoi(b[i+1]);
-            return t;
+            return atoi(b[i+1]);
         }
+
     }
+
+}
+
+void message()
+{
+    cout << "Enter choice" << endl;
+    cout << "1-Rock" << endl;
+    cout << "2-Scissors" << endl;
+    cout << "3-Paper" << endl;
+    cout << "Your choice: ";
 }
 
 int main(int argc, char *argv[])
 {
-    string choise;
-
+   enum class Choise{Rock = 1, Scissors, Paper};
+    int choice;
     int num = number(argc, argv);
     for (int i=0; i<num;i++)
     {
-        cout << "Enter Rock, Scissors or Paper: ";
-        cin >> choise;
-        RSP(choise);
+        message();
+        cin >> choice;
+        RSP(choice);
         system("pause");
         system("cls");
     }
